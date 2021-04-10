e0f4533f0f66fff076bd0af3d334b41e10f74f80
HaYeonKim0807 <kh701201@naver.com>
2021-04-10 15:36:33 +0900

    Hello HaYeon

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            Player player = new Player();
+
+            board.Initialize(25, player);
+            player.Initialize(1, 1, board);
+
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+            
+
+            int lastTick = 0;
+                while (true)
+                {
+                    int currentTick = System.Environment.TickCount;
+
+                    if (currentTick - lastTick < WAIT_TICK)
+                    {
+                        continue;
+                    }
+
+                    int deltaTick = currentTick - lastTick;
+                    lastTick = currentTick;
+
+                    player.Update(deltaTick+80);
+
+                    Console.SetCursorPosition(0, 0);
+                    board.Render();
+                
+                }
+        }
+    }
+}
